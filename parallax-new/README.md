d26754fd33e5419dd180b5bd7aacc8f5ea6b56ca
Oli Lucas <Oli.Lucas@thinkorchard.com>
2019-08-15 17:05:45 +0100

    dot trail for glow tip

--- parallax-new/README.md
+++ parallax-new/README.md
@@ -1,5 +1,3 @@
-# Webpack 4 Boilerplate ES6/Sass with build-in option to change preprocessor (less/stylus)
-This <strong>Webpack 4 Boilerplate</strong> comes with 2 builds:
 
 ```
 npm run build:dev
@@ -11,7 +9,7 @@ npm run build:prod
 ```
 -> creates prod files to <code>/dist</code> with:
 
-  1. compiles sass/stylus/less to css <br>
+  1. compiles less to css <br>
   2. autoprefixer for vendor prefixes (browser compability) <br>
   3. compiles ES6+ to ES5 <br>
   4. minifying for css/js <br>
@@ -19,29 +17,7 @@ npm run build:prod
   6. hash css and js file (file versioning for browser caching -> cache busting) <br>
 
 ## Setup
-1. git clone https://github.com/mwieth/Webpack-4-boilerplate.git
-2. run <code>npm install</code> in project folder
-3. <code>npm run build:dev</code>
+1. run <code>npm install</code> in project folder
+2. <code>npm run build:dev</code>
 
-## Preprocessor support (default: Sass)
 
---> if u want to change to <strong>less</strong> run:
-
-  1. <code>npm install less less-loader --save-dev</code>
-  2. <code>npm uninstall node-sass sass-loader</code>
-
-  3. set <code>selectedPreprocessor</code> in \webpack\loader.js to less
-
-  4. change default files in styles from sass to less (*.less) and update <code>import</code> in index.js line 1
-
---> if u want to change to <strong>stylus</strong> run:
-
-  1. <code>npm install stylus stylus-loader --save-dev</code>
-  2. <code>npm uninstall node-sass sass-loader</code>
-
-  3. set <code>selectedPreprocessor</code> in \webpack\loader.js to stylus
-
-  4. change default files in styles from sass to stylus (*.styl) update <code>import</code> in index.js line 1
-
---> if u want to use the 'original' loose <strong>*.sass</strong> syntax just change the files from
-*.scss to *.sass and update <code>import</code> in index.js line 1
--- parallax-new/src/js/scroll-magic.js
+++ parallax-new/src/js/scroll-magic.js
@@ -24,23 +24,22 @@ export default {
         //glow tip path
         var glowTipPath = {
 			entry : {
-				curviness: 1.25,
+				curviness: 0,
 				autoRotate: true,
 				values: [
-						{x: 0,	y: 0},
-                        {x: 30,	y: 10}]
-
-			}
+						{x: 10,	y: 10},
+                        {x: 130,	y: 10}]
+			},
 			
-			// leave : {
-			// 	curviness: 1.25,
-			// 	autoRotate: true,
-			// 	values: [
-			// 			{x: 60,	y: 20},
-			// 			// {x: 80,	y: 10},
-			// 			// {x: $(window).width() + 300,	y: -100},
-			// 		]
-			// }
+			leave : {
+				curviness: 0,
+				autoRotate: true,
+				values: [
+						{x: 60,	y: 20},
+						{x: 80,	y: 10},
+                        //{x: $(window).width() + 300,	y: -100},
+					]
+			}
 		};
         // init controller
         var controller = new ScrollMagic.Controller();
@@ -48,22 +47,36 @@ export default {
         // build tween
 
         var tweenGlowTip = new TimelineMax()
-        //    .add(TweenMax.to($("#glow-tip"), 1.2, {css:{bezier:glowTipPath.entry}, ease:Power1.easeInOut}))
+            .add(TweenMax.to($("#glow-tip"), 1.2, {css:{bezier:glowTipPath.entry}, ease:Linear.easeNone}))
            // .add(TweenMax.to($("#glow-tip"), 1, {css:{bezier:glowTipPath.leave}, ease:Power1.easeInOut}));
+
+
+           var dots = 200;
+           var target = $("#glow-tip")[0]
+             
+           for (var i = 1; i <= dots; i++){
+            var dot = $("<div class='dot'></div>").appendTo($("#target")); //create a dot.
+            tweenGlowTip.progress(i/dots); // increment the progress().
+            
+           TweenLite.set(dot, {x:target._gsTransform.x, y:target._gsTransform.y, rotation:target._gsTransform.rotation, opacity:0}); //position dot based on target's x/y position.
+           tweenGlowTip.set(dot, {opacity:1}, tweenGlowTip.time())
+           tweenGlowTip.time(0).play()
+           }
+             
 
         var tweenScene2Line = new TimelineMax()
             .add(TweenMax.to($scene2Anim, 1.2, {strokeDashoffset: 0, ease:Linear.easeNone})) // draw word for 0.9
             // .add(TweenMax.to($word, 0.1, {strokeDashoffset: 0, ease:Linear.easeNone}))  // draw dot for 0.1
             .add(TweenMax.to("path", 1, {stroke: "#ffffff", ease:Linear.easeNone}), 0);	// change color during the whole thing
-    
+
         // build scene
-        // var scene = new ScrollMagic.Scene({triggerElement: "#scene2", duration: 400, tweenChanges: true})
-        //                 .setPin("#target")
-        //                 .setTween(tweenGlowTip)
-        //                 .addIndicators() // add indicators (requires plugin)
-        //                 .addTo(controller);
-        
-                        // build scene
+        var scene = new ScrollMagic.Scene({triggerElement: "#scene2", duration: 400, tweenChanges: true})
+                        .setPin("#target")
+                        .setTween(tweenGlowTip)
+                        .addIndicators() // add indicators (requires plugin)
+                        .addTo(controller);
+
+        // build scene
         var scene1 = new ScrollMagic.Scene({triggerElement: "#scene2", duration: 400, tweenChanges: true})
                         .setTween(tweenScene2Line)
                         .addIndicators() // add indicators (requires plugin)
